83ab8c144dc6270970583cb76590ccbc3aefa827
Jordy Garcia Mendoza <jordy0914@hotmail.com>
2016-06-14 15:58:19 -0600

    cambios de clases frmAgregarZapatos

--- prgNegocioDeZapatos/prgNegocioDeZapatos/frmAgregarZapatos.cs
+++ prgNegocioDeZapatos/prgNegocioDeZapatos/frmAgregarZapatos.cs
@@ -7,13 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace prgNegocioDeZapatos
 {
-    public partial class frmAgregarZapatos : Form
+    public partial class frmAgregarZapatos : MaterialForm
     {
+        private readonly MaterialSkinManager materialSkinManager;
         public frmAgregarZapatos()
         {
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.DeepOrange700, Primary.DeepOrange900, Primary.DeepOrange500, Accent.DeepOrange200, TextShade.WHITE);
+
             InitializeComponent();
         }
 
@@ -29,6 +37,8 @@ namespace prgNegocioDeZapatos
         {
             Close();
         }
+
+     
     }
 }
 
