0a14528bf4e65241b015e5bd1f37cad0f7e949d6
acesp_000 <acesp_000@TATIANA>
2016-06-20 15:12:50 -0600

    hygf

--- prgNegocioDeZapatos/prgNegocioDeZapatos/frmAsignarVentanaRol.Designer.cs
+++ prgNegocioDeZapatos/prgNegocioDeZapatos/frmAsignarVentanaRol.Designer.cs
@@ -237,7 +237,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(901, 404);
+            this.ClientSize = new System.Drawing.Size(703, 404);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnEliminar);
