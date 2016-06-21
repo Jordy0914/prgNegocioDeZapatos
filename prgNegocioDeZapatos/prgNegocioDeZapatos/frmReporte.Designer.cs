3c9758c0162d29aed07df0f16562ce2e74083a6e
acesp_000 <acesp_000@TATIANA>
2016-06-21 16:17:10 -0600

    Creacion de frm Inventario

---
+++ prgNegocioDeZapatos/prgNegocioDeZapatos/frmReporte.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace prgNegocioDeZapatos
+{
+    partial class Inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsvReporteZapatos = new MaterialSkin.Controls.MaterialListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSalir = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SuspendLayout();
+            // 
+            // lsvReporteZapatos
+            // 
+            this.lsvReporteZapatos.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lsvReporteZapatos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lsvReporteZapatos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10,
+            this.columnHeader11});
+            this.lsvReporteZapatos.Depth = 0;
+            this.lsvReporteZapatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lsvReporteZapatos.FullRowSelect = true;
+            this.lsvReporteZapatos.GridLines = true;
+            this.lsvReporteZapatos.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lsvReporteZapatos.Location = new System.Drawing.Point(12, 92);
+            this.lsvReporteZapatos.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.lsvReporteZapatos.MouseState = MaterialSkin.MouseState.OUT;
+            this.lsvReporteZapatos.Name = "lsvReporteZapatos";
+            this.lsvReporteZapatos.OwnerDraw = true;
+            this.lsvReporteZapatos.Size = new System.Drawing.Size(925, 196);
+            this.lsvReporteZapatos.TabIndex = 0;
+            this.lsvReporteZapatos.UseCompatibleStateImageBehavior = false;
+            this.lsvReporteZapatos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Código zapato";
+            this.columnHeader1.Width = 127;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Código Cliente";
+            this.columnHeader2.Width = 124;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Modelo";
+            this.columnHeader3.Width = 79;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Nombre";
+            this.columnHeader4.Width = 85;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Talla";
+            this.columnHeader5.Width = 70;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Colo";
+            this.columnHeader6.Width = 50;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Costo";
+            this.columnHeader7.Width = 65;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Precio";
+            this.columnHeader8.Width = 69;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Marca";
+            this.columnHeader9.Width = 72;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Categoria";
+            this.columnHeader10.Width = 93;
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "Cantidad";
+            this.columnHeader11.Width = 79;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.AutoSize = true;
+            this.btnSalir.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSalir.Depth = 0;
+            this.btnSalir.Location = new System.Drawing.Point(852, 297);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnSalir.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Primary = false;
+            this.btnSalir.Size = new System.Drawing.Size(49, 36);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 359);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lsvReporteZapatos);
+            this.Name = "Inventario";
+            this.Text = "frmReporte";
+            this.Load += new System.EventHandler(this.frmReporte_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialListView lsvReporteZapatos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private MaterialSkin.Controls.MaterialFlatButton btnSalir;
+    }
+}
