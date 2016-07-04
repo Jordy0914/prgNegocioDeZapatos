a0b78de10977e770608f5466aa8c89e72dbfae08
Jordy Garcia Mendoza <jordy0914@hotmail.com>
2016-07-04 16:14:27 -0600

    sdf

--- prgNegocioDeZapatos/prgNegocioDeZapatos/frmModificarRolesVistas.cs
+++ prgNegocioDeZapatos/prgNegocioDeZapatos/frmModificarRolesVistas.cs
@@ -11,31 +11,88 @@ using System.Windows.Forms;
 using MaterialSkin;
 using MaterialSkin.Controls;
 
+using System.Data.SqlClient;
+using Entidades;
+using AccesoDatos;
+using LogicaNegocios;
+
 namespace prgNegocioDeZapatos
 {
     public partial class frmModificarRolesVistas : MaterialForm
     {
         #region Atributos
         private readonly MaterialSkinManager materialSkinManager;
+
+        private clsConexion conexion;
+
+        private clsRolVista clRolVista;
+
+        private SqlDataReader dtr;
+
+        public int IdRol { get; set; }
+        public String RolName{ get; set; }
+        public int IdVista { get; set; }
+        public String VistaName { get; set; }
         #endregion
 
-        public frmModificarRolesVistas()
+        public frmModificarRolesVistas(clsConexion cone)
         {
             materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.DeepOrange700, Primary.DeepOrange900, Primary.DeepOrange500, Accent.DeepOrange200, TextShade.WHITE);
 
+            this.conexion = cone;
+
+            this.clRolVista = new clsRolVista();
+
             InitializeComponent();
         }
 
         private void frmModificarRolesVistas_Load(object sender, EventArgs e)
         {
+            this.llenarLV();
         }
-
+        #region Eventos
         private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        private void lvLista_DoubleClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lvLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lvGeneral.Items.Count; i++)
+            {
+                if (lvGeneral.Items[i].Selected)
+                {
+                    IdRol = Convert.ToInt32(lvGeneral.Items[i].Text);
+                    RolName = lvGeneral.Items[i].SubItems[1].Text.ToString();
+                    IdVista = Convert.ToInt32(lvGeneral.Items[i].SubItems[2].Text);
+                    VistaName = lvGeneral.Items[i].SubItems[3].Text;
+                }
+            }//fin del for
+        }//fin del metodo
+
+        #endregion
+
+        #region Metodos Propios
+        public void llenarLV()
+        {
+            dtr = clRolVista.mConsultarRolesVistasGeneral(conexion);
+            while (dtr.Read())
+            {
+                ListViewItem lista;
+                lista = lvGeneral.Items.Add(Convert.ToString(dtr.GetInt32(0)));
+                lista.SubItems.Add(dtr.GetString(1));
+                lista.SubItems.Add(Convert.ToString(dtr.GetInt32(2)));
+                lista.SubItems.Add(dtr.GetString(3));
+            }//fin del while
+        }
+        #endregion
     }
 }
